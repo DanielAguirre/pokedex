2c6d65bcab010da2dea62bcd8def0577f89c22ba
DanielAguirre <danter1@gmail.com>
2015-08-08 12:41:56 -0500

    Add module express server with initial configuration

---
+++ expressServer/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const path = require('path')
+const express = require('express')
+const swig = require('swig')
+
+
+function ExpressServer (){
+	this.expressServer = express()
+
+	//middlewares
+	this.expressServer.use(bodyParser.json())
+	this.expressServer.use(bodyParser.urlencoded({ extended: false }))
+	this.expressServer.use(express.static(path.join(__dirname,'..', 'public')))
+
+	//template Engine
+	this.expressServer.engine('html',swig.renderFile)
+	this.expressServer.set('view engine','html')
+	this.expressServer.set('views',path.join(__dirname,'..','public'))
+
+	for(var url in urls){
+		this.expressServer.use(url,urls[url])
+	}
+}
+
+module.exports = ExpressServer
